7dbf6171c971d6aa069a291156bba1918dc5332b
havenfricke <havenfricke@u.boisestate.edu>
2026-05-27 17:04:50 -0600

    reverted css

--- src/Components/ModalWindow.jsx
+++ src/Components/ModalWindow.jsx
@@ -75,8 +75,8 @@ export default function ModalWindow({ modalId, title, children }) {
     <>
       {/* The trigger remains in the regular tree */}
       <div className='view-project-button' ref={openBtnRef} id={`${uniqueId}-trigger`} style={{ cursor: 'pointer' }} title='View' tabIndex={0}>
-        <span className="hover-text">VIEW</span>
-        <img className="right-arrow-card-button" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/>
+        {/* <span className="hover-text">VIEW</span> */}
+        {/* <img className="right-arrow-card-button" src="/arrow-right-solid.svg" alt="Right Arrow" data-skip-on-tab="true"/> */}
         <span className='button-title'>{title}</span>
       </div>
 
